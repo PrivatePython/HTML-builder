b997445eeecd919adf3b1eb545972c9a7e672ba7
privatePython <gitarist.pasha@gmail.com>
2025-01-21 02:37:18 +0300

    fix: fix bug with error in console, when program run first time, this is 05-merge-styles task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,6 @@
-const { readdir } = require('fs/promises');
+const { readdir, readFile } = require('fs/promises');
 const path = require('path');
-const { readFile, createWriteStream } = require('node:fs');
+const { createWriteStream } = require('node:fs');
 
 const folderToAddBundle = path.join(__dirname, 'project-dist');
 const folderForCopy = path.join(__dirname, 'styles');
@@ -14,19 +14,17 @@ readdir(folderForCopy, {
     );
     arrayDirectObjects
       .filter((item) => item.isFile() && path.parse(item.name).ext === '.css')
-      .forEach((directObject, index, array) => {
+      .forEach(async (directObject, index, array) => {
         const isLastChunk = array.length - 1 === index;
         const pathFileCss = path.join(directObject.path, directObject.name);
-        readFile(pathFileCss, (err, data) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          writableStream.write(`${data}${isLastChunk ? '' : '\n'}`);
-          if (isLastChunk) {
-            writableStream.close();
-          }
-        });
+        await readFile(pathFileCss)
+          .then((data) => {
+            writableStream.write(`${data}${isLastChunk ? '' : '\n'}`);
+          })
+          .catch((e) => console.error(e));
+        if (isLastChunk) {
+          writableStream.close();
+        }
       });
   })
   .catch((e) => console.error(e));
